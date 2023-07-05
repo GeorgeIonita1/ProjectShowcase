b37ef63e0e4d790f51db14d007e8218d6e57f4d1
George <g3orrg301@gmail.com>
2023-07-05 12:59:23 +0300

    Changed credentials namine

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
 
 const firebaseConfig = {
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
 };
 
 admin.initializeApp({
