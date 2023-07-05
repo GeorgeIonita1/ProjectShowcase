224ea32d6c6dd542781a0f8d0773936c82a59ade
George <g3orrg301@gmail.com>
2023-07-05 14:14:53 +0300

    key formatting

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3331);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
